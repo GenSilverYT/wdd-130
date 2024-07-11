fec7e063ecf12b44cda9a3c71293d6ad24d923cd
GenSilverYT <asherdecrescenzo@gmail.com>
2024-07-11 17:36:32 -0600

    new file:   php_files/quiz-results.php modified:   quiz.html

---
+++ php_files/quiz-results.php
@@ -0,0 +1,24 @@
+<script>
+    function checkAnswers() {
+        var correctAnswers = ["a", "a", "b", "b"]; // Replace with correct answers
+    
+        var form = document.getElementById("quizForm");
+        var score = 0;
+    
+        for (var i = 0; i < form.elements.length - 1; i++) { // -1 to exclude the submit button
+            var element = form.elements[i];
+            if (element.type === "radio" && element.checked) {
+                if (element.value === correctAnswers[i]) {
+                    score++;
+                    element.parentNode.style.color = "green"; // Mark correct answer
+                } else {
+                    element.parentNode.style.color = "red"; // Mark incorrect answer
+                }
+            }
+        }
+    
+        alert("You scored " + score + " out of " + correctAnswers.length);
+    
+        return false; // Prevent form submission
+    }
+    </script>
